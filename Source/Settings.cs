1f1fb7f82c5099834ec56b448cb717814037491e
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2024-09-29 15:20:31 -0500

    Use DivineFramework for settings

--- Source/Settings.cs
+++ Source/Settings.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using Verse;
+using DivineFramework;
 
 namespace NonBinaryGender
 {
@@ -12,12 +13,47 @@ namespace NonBinaryGender
         public GenderNeutralNameOption neutralNames = GenderNeutralNameOption.None;
 
         public static bool HARActive = false;
+        internal static SettingsHandler<Settings> handler = new();
 
         public override void ExposeData()
         {
             base.ExposeData();
             Scribe_Values.Look(ref enbyChance, "enbyChance", 0.1f);
             Scribe_Values.Look(ref neutralNames, "neutralNames", GenderNeutralNameOption.None);
+        }
+
+        internal void SetUpHandler()
+        {
+            handler.RegisterNewRow().AddLabel(() => "Enby.EnbyChance".Translate() + (int)(enbyChance * 100f) + "%");
+            handler.RegisterNewRow()
+                .AddElement(NewElement.Slider<Settings, float>()
+                .WithReference(this, "enbyChance", enbyChance)
+                .MinMax(0f, 1f), "ChanceSlider");
+            UIContainer buttonRow = handler.RegisterNewRow("ButtonRow", 2f);
+            buttonRow.AddLabel("Enby.GenderNeutralNames".Translate)
+                .AddTooltip("Enby.GenderNeutralNamesTooltip".Translate);
+            buttonRow.AddElement(NewElement.Button(GenderNeutralButtonAction)
+                .WithReference(this, "neutralNames", neutralNames)
+                .WithLabel(() => neutralNames.ToString()));
+            handler.RegisterNewRow().AddLabel(() => Names.OptionTooltip(neutralNames, true));
+
+            handler.Initialize();
+        }
+
+        private void GenderNeutralButtonAction()
+        {
+            List<FloatMenuOption> options = [];
+            foreach (GenderNeutralNameOption value in Enum.GetValues(typeof(GenderNeutralNameOption)))
+            {
+                options.Add(new FloatMenuOption(Enum.GetName(typeof(GenderNeutralNameOption), value), delegate
+                {
+                    neutralNames = value;
+                })
+                {
+                    tooltip = Names.OptionTooltip(value, false)
+                });
+            }
+            Find.WindowStack.Add(new FloatMenu(options));
         }
     }
 
@@ -52,25 +88,12 @@ namespace NonBinaryGender
             };
             list.Begin(canvas);
 
-            list.Label("Enby.EnbyChance".Translate() + (int)(settings.enbyChance * 100f) + "%");
-            settings.enbyChance = list.Slider(settings.enbyChance, 0f, 1f);
-
-            if (list.ButtonTextLabeled("Enby.GenderNeutralNames".Translate(), settings.neutralNames.ToString(), tooltip: "Enby.GenderNeutralNamesTooltip".Translate()))
+            if (!Settings.handler.Initialized)
             {
-                List<FloatMenuOption> options = [];
-                foreach (GenderNeutralNameOption value in Enum.GetValues(typeof(GenderNeutralNameOption)))
-                {
-                    options.Add(new FloatMenuOption(Enum.GetName(typeof(GenderNeutralNameOption), value), delegate
-                    {
-                        settings.neutralNames = value;
-                    })
-                    {
-                        tooltip = Names.OptionTooltip(value, false)
-                    });
-                }
-                Find.WindowStack.Add(new FloatMenu(options));
+                Settings.handler.width = list.ColumnWidth;
+                settings.SetUpHandler();
             }
-            list.Label(Names.OptionTooltip(settings.neutralNames, true));
+            Settings.handler.Draw(list);
 
             list.End();
         }
