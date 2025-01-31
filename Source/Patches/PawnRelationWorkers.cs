e2e9aa849434a95df2ef16aad29ccd7046d7b7a6
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2025-01-31 14:59:44 -0600

    Some patches no longer needed
    due to previous change

--- Source/Patches/PawnRelationWorkers.cs
+++ Source/Patches/PawnRelationWorkers.cs
@@ -53,128 +53,12 @@ namespace NonBinaryGender.Patches
         }
 
         [HarmonyPostfix]
-        [HarmonyPatch(typeof(PawnRelationWorker_Child), nameof(PawnRelationWorker_Child.InRelation))]
-        public static void ChildPostfix(Pawn me, Pawn other, ref bool __result)
-        {
-            //If it's already true we don't need to do anything
-            if (!__result && me != other)
-            {
-                //In case they're both enby, we need to compare them both
-                __result = other.GetParents().Where(p => p.IsEnby()).Any(p => p == me);
-            }
-        }
-
-        [HarmonyPostfix]
         [HarmonyPatch(typeof(PawnRelationWorker_UncleOrAunt), nameof(PawnRelationWorker_UncleOrAunt.InRelation))]
         public static void UncleOrAuntPostfix(Pawn me, Pawn other, ref bool __result)
         {
             if (!__result && me != other)
             {
                 __result = InRelationViaParents(me, other, PawnRelationDefOf.Sibling.Worker) || InRelationViaParents(me, other, PawnRelationDefOf.HalfSibling.Worker);
-            }
-        }
-
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(PawnRelationWorker_NephewOrNiece), nameof(PawnRelationWorker_NephewOrNiece.InRelation))]
-        public static void NephewOrNiecePostfix(Pawn me, Pawn other, ref bool __result)
-        {
-            if (!__result && me != other)
-            {
-                __result = InRelationViaParents(other, me, PawnRelationDefOf.Sibling.Worker) || InRelationViaParents(other, me, PawnRelationDefOf.HalfSibling.Worker);
-            }
-        }
-
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(PawnRelationWorker_Grandchild), nameof(PawnRelationWorker_Grandchild.InRelation))]
-        public static void GrandchildPostfix(Pawn me, Pawn other, ref bool __result)
-        {
-            if (!__result && me != other)
-            {
-                __result = InRelationViaParents(other, me, PawnRelationDefOf.Child.Worker);
-            }
-        }
-
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(PawnRelationWorker_Cousin), nameof(PawnRelationWorker_Cousin.InRelation))]
-        public static void CousinPostfix(Pawn me, Pawn other, ref bool __result)
-        {
-            if (!__result && me != other)
-            {
-                __result = InRelationViaParents(other, me, PawnRelationDefOf.UncleOrAunt.Worker);
-            }
-        }
-
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(PawnRelationWorker_CousinOnceRemoved), nameof(PawnRelationWorker_CousinOnceRemoved.InRelation))]
-        public static void CousinOnceRemovedPostfix(Pawn me, Pawn other, ref bool __result)
-        {
-            if (!__result && me != other)
-            {
-                __result = InRelationViaParents(other, me, PawnRelationDefOf.Cousin.Worker) || InRelationViaParents(other, me, PawnRelationDefOf.GranduncleOrGrandaunt.Worker);
-            }
-        }
-
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(PawnRelationWorker_GrandnephewOrGrandniece), nameof(PawnRelationWorker_GrandnephewOrGrandniece.InRelation))]
-        public static void GrandnephewOrGrandniecePostfix(Pawn me, Pawn other, ref bool __result)
-        {
-            if (!__result && me != other)
-            {
-                __result = InRelationViaParents(other, me, PawnRelationDefOf.NephewOrNiece.Worker);
-            }
-        }
-
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(PawnRelationWorker_GranduncleOrGrandaunt), nameof(PawnRelationWorker_GranduncleOrGrandaunt.InRelation))]
-        public static void GranduncleOrGrandauntPostfix(Pawn me, Pawn other, ref bool __result)
-        {
-            if (!__result && me != other)
-            {
-                __result = InRelationViaParents(other, me, PawnRelationDefOf.GreatGrandparent.Worker);
-            }
-        }
-
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(PawnRelationWorker_GreatGrandchild), nameof(PawnRelationWorker_GreatGrandchild.InRelation))]
-        public static void GreatGrandchildPostfix(Pawn me, Pawn other, ref bool __result)
-        {
-            if (!__result && me != other)
-            {
-                __result = InRelationViaParents(other, me, PawnRelationDefOf.Grandchild.Worker);
-            }
-        }
-
-        //Prefix because we don't just need to look at their enby parent's enby parents, but binary parents of their enby parents and enby parents of their binary parents
-        //So might as well just look at them all and be done
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(PawnRelationWorker_SecondCousin), nameof(PawnRelationWorker_SecondCousin.InRelation))]
-        public static bool SecondCousinPrefix(Pawn me, Pawn other, ref bool __result)
-        {
-            if (me != other)
-            {
-                foreach (Pawn parent in other.GetParents())
-                {
-                    if (InRelationViaParents(parent, me, PawnRelationDefOf.GranduncleOrGrandaunt.Worker, false))
-                    {
-                        __result = true;
-                        return false;
-                    }
-                }
-            }
-            return false;
-        }
-
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(PawnRelationWorker_Stepparent), nameof(PawnRelationWorker_Stepparent.InRelation))]
-        public static void StepparentPostfix(Pawn me, Pawn other, ref bool __result)
-        {
-            if (!__result && me != other)
-            {
-                //Make sure they're not already a parent
-                if (!PawnRelationDefOf.Parent.Worker.InRelation(me, other))
-                {
-                    __result = InRelationViaParents(me, other, PawnRelationDefOf.Spouse.Worker);
-                }
             }
         }
 
