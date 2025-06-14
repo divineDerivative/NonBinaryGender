fc49ecbf3da2354f6d9371e8a7c2d5000e8378e8
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2025-06-14 18:21:21 -0500

    Update for 1.6

--- Source/Patches/PawnGenerator.cs
+++ Source/Patches/PawnGenerator.cs
@@ -30,6 +30,8 @@ namespace NonBinaryGender.Patches
             }
         }
 
+        //1.4 only version of the transpiler because it seems like Harmony doesn't have all the stuff I'm using in that version
+#if v1_4
         //This inserts a check against the enby chance when choosing a new pawn's gender
         [HarmonyTranspiler]
         [HarmonyPatch(typeof(PawnGenerator), "TryGenerateNewPawnInternal")]
@@ -37,17 +39,71 @@ namespace NonBinaryGender.Patches
         {
             MethodInfo RaceProps = typeof(Pawn).GetProperty("RaceProps").GetGetMethod();
             MethodInfo Animal = typeof(RaceProperties).GetProperty("Animal").GetGetMethod();
-            Label startMyStuff = generator.DefineLabel();
+            Label? doneWithGender = new Label();
+            Label notAnimalorEnby = generator.DefineLabel();
             bool done = false;
 
+            List<CodeInstruction> codes = instructions.ToList();
+            for (int i = 0; i < codes.Count; i++)
+            {
+                CodeInstruction code = codes[i];
+                CodeInstruction nextCode = codes[i + 1];
+                //We look right after gender is assigned in a previous section to see where it jumps to
+                if (code.StoresField(InfoHelper.genderField) && nextCode.Branches(out doneWithGender))
+                {
+                    break;
+                }
+            }
+
+            foreach (CodeInstruction code in instructions)
+            {
+                //Look for Rand.Value being called, then insert our stuff before it
+                if (!done && code.Calls(InfoHelper.RandValue))
+                {
+                    //We check for animals because we don't want non-binary cows
+                    //if (!pawn.RaceProps.Animal
+                    yield return new CodeInstruction(OpCodes.Ldloc_0);
+                    yield return new CodeInstruction(OpCodes.Callvirt, RaceProps);
+                    yield return new CodeInstruction(OpCodes.Callvirt, Animal);
+                    yield return new CodeInstruction(OpCodes.Brtrue, notAnimalorEnby);
+                    //Check against the enby chance
+                    //&& Rand.Value < NonBinaryGenderMod.settings.enbyChance)
+                    yield return new CodeInstruction(OpCodes.Call, InfoHelper.RandValue);
+                    yield return CodeInstruction.LoadField(typeof(NonBinaryGenderMod), nameof(NonBinaryGenderMod.settings));
+                    yield return CodeInstruction.LoadField(typeof(Settings), nameof(Settings.enbyChance));
+                    yield return new CodeInstruction(OpCodes.Bge_Un, notAnimalorEnby);
+                    //pawn.gender = 3;
+                    yield return new CodeInstruction(OpCodes.Ldloc_0);
+                    yield return new CodeInstruction(OpCodes.Ldc_I4_3);
+                    yield return new CodeInstruction(OpCodes.Stfld, InfoHelper.genderField);
+                    yield return new CodeInstruction(OpCodes.Br_S, doneWithGender);
+                    //Add our label so it jumps to here if the pawn is an animal or enby was not selected
+                    code.labels.Add(notAnimalorEnby);
+                    done = true;
+                }
+                //Then continue as usual
+                yield return code;
+            }
+        }
+#endif
+#if !v1_4
+        //This inserts a check against the enby chance when choosing a new pawn's gender
+        [HarmonyTranspiler]
+        [HarmonyPatch(typeof(PawnGenerator), "TryGenerateNewPawnInternal")]
+        [HarmonyDebug]
+        public static IEnumerable<CodeInstruction> TryGenerateNewPawnInternalTranspiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            MethodInfo RaceProps = typeof(Pawn).GetProperty("RaceProps").GetGetMethod();
+            MethodInfo Animal = typeof(RaceProperties).GetProperty("Animal").GetGetMethod();
+            Label startMyStuff = generator.DefineLabel();
+
             CodeMatcher matcher = new CodeMatcher(instructions, generator)
-                     .MatchEndForward(
-            CodeMatch.StoresField(InfoHelper.genderField),
-            CodeMatch.Branches()
-        );
+                .MatchEndForward(
+                CodeMatch.StoresField(InfoHelper.genderField),
+                CodeMatch.Branches()
+                );
             Label doneWithGender = (Label)matcher.Operand;
 
-#if !v1_4
             //Get the compiler generated type that stores the pawn
             Type compilerType = null;
             IEnumerable<Type> types = AccessTools.InnerTypes(typeof(PawnGenerator));
@@ -64,38 +120,45 @@ namespace NonBinaryGender.Patches
                     }
                 }
             }
+#if v1_6
+            //Need to find the new branch and have it jump to my code
+            matcher.MatchEndForward(
+                CodeMatch.LoadsField(AccessTools.Field(typeof(RaceProperties), nameof(RaceProperties.forceGender))),
+                CodeMatch.Branches()
+                );
+
+            //Grab the label for jumping to the Rand.Value call
+            Label startRandom = (Label)matcher.Operand;
+            //Change it to jump to my section
+            matcher.SetOperandAndAdvance(startMyStuff);
 #endif
             //Look for Rand.Value being called, then insert our stuff before it
             matcher.MatchStartForward(CodeMatch.Calls(InfoHelper.RandValue))
                 //Add our label so it jumps to here if the pawn is an animal or enby was not selected
-                .CreateLabel(out Label notAnimalorEnby)
-                .Insert(
-                    //We check for animals because we don't want non-binary cows
-                    //if (!pawn.RaceProps.Animal
-                    new CodeInstruction(OpCodes.Ldloc_0).WithLabels(startMyStuff),
-#if !v1_4
-                    CodeInstruction.LoadField(compilerType, "pawn"),
-#endif
-                    new CodeInstruction(OpCodes.Callvirt, RaceProps),
-                    new CodeInstruction(OpCodes.Callvirt, Animal),
-                    new CodeInstruction(OpCodes.Brtrue, notAnimalorEnby),
-                    //Check against the enby chance
-                    //&& Rand.Value < NonBinaryGenderMod.settings.enbyChance)
-                    new CodeInstruction(OpCodes.Call, InfoHelper.RandValue),
-                    CodeInstruction.LoadField(typeof(NonBinaryGenderMod), nameof(NonBinaryGenderMod.settings)),
-                    CodeInstruction.LoadField(typeof(Settings), nameof(Settings.enbyChance)),
-                    new CodeInstruction(OpCodes.Bge_Un, notAnimalorEnby),
-                    //pawn.gender = 3;
-                    new CodeInstruction(OpCodes.Ldloc_0),
-#if !v1_4
-                    CodeInstruction.LoadField(compilerType, "pawn"),
-#endif
-                    new CodeInstruction(OpCodes.Ldc_I4_3),
-                    new CodeInstruction(OpCodes.Stfld, InfoHelper.genderField),
-                    new CodeInstruction(OpCodes.Br_S, doneWithGender)
-                );
+                .CreateLabel(out Label notAnimalorEnby);
+
+            matcher.Insert(
+                //We check for animals because we don't want non-binary cows
+                //if (!pawn.RaceProps.Animal
+                new CodeInstruction(OpCodes.Ldloc_0).WithLabels(startMyStuff),
+                new CodeInstruction(OpCodes.Callvirt, RaceProps),
+                new CodeInstruction(OpCodes.Callvirt, Animal),
+                new CodeInstruction(OpCodes.Brtrue, notAnimalorEnby),
+                //Check against the enby chance
+                //&& Rand.Value < NonBinaryGenderMod.settings.enbyChance)
+                new CodeInstruction(OpCodes.Call, InfoHelper.RandValue),
+                CodeInstruction.LoadField(typeof(NonBinaryGenderMod), nameof(NonBinaryGenderMod.settings)),
+                CodeInstruction.LoadField(typeof(Settings), nameof(Settings.enbyChance)),
+                new CodeInstruction(OpCodes.Bge_Un, notAnimalorEnby),
+                //pawn.gender = 3;
+                new CodeInstruction(OpCodes.Ldloc_0),
+                new CodeInstruction(OpCodes.Ldc_I4_3),
+                new CodeInstruction(OpCodes.Stfld, InfoHelper.genderField),
+                new CodeInstruction(OpCodes.Br_S, doneWithGender)
+            );
             return matcher.InstructionEnumeration();
         }
+#endif
 
         //This changes the gender check after the 50% chance to get thin to be another 50% chance to get male or female, instead of defaulting to male
         [HarmonyTranspiler]
