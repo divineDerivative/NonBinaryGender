261d028df1769707aa5d3a16500e1e2fe42c6c54
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2025-08-17 14:47:18 -0500

    DivineFramework 0.12.1.1

--- Source/Properties/FrameworkVersion.g.cs
+++ Source/Properties/FrameworkVersion.g.cs
@@ -2,6 +2,6 @@ namespace NonBinaryGender
 {
     internal static class FrameworkVersionInfo
     {
-        public const string Version = "0.12.0.1";
+        public const string Version = "0.12.1.1";
     }
 }
